b0d46bbac717b65b8fd79e57868694b7ef56e6f0
mani-2002 <110903520+mani-2002@users.noreply.github.com>
2023-07-30 00:49:17 +0530

    Add files via upload

---
+++ 2) Modal popup/js/script.js
@@ -0,0 +1,9 @@
+function openmodel() {
+    document.getElementById("container").style.display = "block";
+    document.getElementById("popup").style.display = "block";
+}
+
+function cancel() {
+    document.getElementById("container").style.display = "none";
+    document.getElementById("popup").style.display = "none";
+}
